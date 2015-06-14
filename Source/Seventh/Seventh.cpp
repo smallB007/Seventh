849c3066303b12c14987f9a9154f7a8656e734e3
smallB <atch.cpp@gmail.com>
2015-06-14 15:53:10 +0100

    Added comment

--- Source/Seventh/Seventh.cpp
+++ Source/Seventh/Seventh.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "Seventh.h"
-
+/*Adding comment*/
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Seventh, "Seventh" );
  
